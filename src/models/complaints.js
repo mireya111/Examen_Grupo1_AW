7e81b603164d19d76891d92ee1e670aef2a33c4a
Mireya Josebeth García Rodríguez <151254789+mireya111@users.noreply.github.com>
2024-11-24 20:47:11 -0500

    Add files via upload

---
+++ src/models/complaints.js
@@ -0,0 +1,85 @@
+//Modelo para denuncias
+const complainModel = {
+    async registerComplaint(newComplain){
+        const url = "http://localhost:4000/complaints";
+        const peticion = await fetch(url, {
+            method:'POST',
+            body:JSON.stringify(newComplain),
+            headers:{'Content-Type':'applicatio/json'}
+        })
+
+        console.log(peticion);
+        const data = await peticion.json();
+
+        return data;
+    },
+
+
+    async getComplaint(){
+        try {
+            const url = "http://localhost:4000/complaints";
+            const peticion = await fetch(url);
+            const data = await peticion.json();
+            return data;
+        } catch (error) {
+            console.error(error)
+        }
+    },
+
+
+    async findComplaint(idComplaint){
+        try {
+            const url = `http://localhost:4000/complaints/${idComplaint}`
+            const peticion = await fetch(url);
+            const data = await peticion.json();
+            return data;
+        } catch (error) {
+            console.error(error)
+        }
+    },
+
+    async updateComplaint(idComplaint, updatedComplaint){
+        try {
+            const url = `http://localhost:4000/complaints/${idComplaint}`
+            const peticion = await fetch(url,{
+                method:'PATCH',
+                body:JSON.stringify(updatedComplaint),
+                headers:{'Content-Type':'application/json'},
+            });
+            const data = await peticion.json();
+            return data;
+        } catch (error) {
+            console.error(error)
+        }
+    },
+
+    async updateComplaintTotally(idComplaint, updatedComplaint){
+        try {
+            const url = `http://localhost:4000/complaints/${idComplaint}`
+            const peticion = await fetch(url,{
+                method:'PUT',
+                body:JSON.stringify(updatedComplaint),
+                headers:{'Content-Type':'application/json'},
+            });
+            const data = await peticion.json();
+            return data;
+        } catch (error) {
+            console.error(error)
+        }
+    },
+
+    async deleteComplaint(idComplaint){
+        try {
+            const url = `http://localhost:4000/complaints/${idComplaint}`
+            const peticion = await fetch(url,{
+                method:'DELETE'
+            });
+            const data = await peticion.json();
+            return data;
+        } catch (error) {
+            console.error(error)
+        }
+    }
+};
+
+export default complainModel;
---
+++ src/models/users.js
@@ -0,0 +1,37 @@
+import bcrypt from 'bcrypt'
+const userModel = {
+
+    async registerUserModel (newUser) {
+        const url = "http://localhost:4000/users"
+        const peticion = await fetch(url,{
+            method:'POST',
+            body:JSON.stringify(newUser),
+            headers:{'Content-Type':'application/json'}
+        })
+
+        console.log(peticion);
+        const data = await peticion.json()
+        
+        return data
+    },
+
+    async loginUserModel (username, password){
+        const url = "http://localhost:4000/users"
+        const response = await fetch(url)
+        const users = await response.json()
+
+        const user = users.find(user => user.username === username)
+        if (!user){
+            return {error: "Usuario o contraseña incorrectos"}
+        }
+        const passwordMatch = await bcrypt.compare(password, user.password)
+        if (user && passwordMatch){
+            return user
+        }else{
+            return {error: "Usuario o contraseña incorrectos"}
+        }
+
+    }
+}
+
+export default userModel
