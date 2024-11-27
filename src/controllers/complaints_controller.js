13c5cf5fd85932693dc08769b6d549819b577b87
PaulCabreaEPN <paul.cabrera@epn.edu.ec>
2024-11-26 19:09:34 -0500

    Coudinary for Reg-Del Complaints

--- src/controllers/complaints_controller.js
+++ src/controllers/complaints_controller.js
@@ -1,5 +1,11 @@
-import { v4 as uuidv4 } from 'uuid'
+//Importe de librerias
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs-extra';
+import { v4 as uuidv4 } from 'uuid';
+
+//Importe de modelos
 import complainModel from '../models/complaints.js';
+
 
 const registerComplaintController = async (req, res) =>{
 
@@ -11,10 +17,16 @@ const registerComplaintController = async (req, res) =>{
     };
 
     try {
+        const cloudinaryResponse = await cloudinary.uploader.upload(req.files.imagen.tempFilePath,{folder:"Denuncias"});
+        complaintData.imagen = cloudinaryResponse.secure_url;
+        complaintData.public_id = cloudinaryResponse.public_id;
+
         const complaint = await complainModel.registerComplaint(complaintData);
         const {id} = complaint;
         res.status(200).json({message:"Denuncia registrada con éxito.", id});
         
+        fs.unlink(req.files.imagen.tempFilePath);
+
     } catch (error) {
         res.status(500).json(error.msg);
     }
@@ -78,7 +90,9 @@ const deleteComplaintController = async (req,res) => {
     const {id} = req.params
 
     try {
-        const complaint = await complainModel.deleteComplaint(id)
+        const complaintFind = await complainModel.findComplaint(id);
+        await cloudinary.uploader.destroy(complaintFind.public_id)
+        await complainModel.deleteComplaint(id);
         res.status(200).json({message:'Denuncia eliminada correctamente'})
     } catch (error) {
         res.status(500).json({message:error})
--- src/server.js
+++ src/server.js
@@ -1,27 +1,45 @@
-//Requerir modulos 
+//Importe Librerias
+import cloudinary from 'cloudinary';
+import fileUpload from 'express-fileupload';
+import dotenv from 'dotenv';
 import express from 'express'
+
+//Requerir Rutas
 import routerUser from './routers/users_routers.js'
 import routerComplaint from './routers/complaints_routers.js'
 
 //Inializaciones 
 const app = express()
 
-app.set('port', process.env.port || 3000)
+app.set('port', process.env.port || 3000);
+
+dotenv.config();
+
+cloudinary.config({ 
+    cloud_name: process.env.cloud_name, 
+    api_key: process.env.cloud_api_key, 
+    api_secret: process.env.cloud_api_secret
+});
+
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: './uploads'
+}));
 
 //  Middleware
-app.use(express.json())
+app.use(express.json());
 
-//Rutas
+//RUTAS
 //Ruta principal
 app.get('/', (req, res)=>{
     res.send("El Servidor del GRUPO 1 está Opreando...")
-})
+});
 
 //Rutas para los usuarios
-app.use('/api', routerUser)
+app.use('/api', routerUser);
 
 //Ruta para las denuncias
-app.use('/api', routerComplaint)
+app.use('/api', routerComplaint);
 
 //Exportación de la instancia app
 export default app;
